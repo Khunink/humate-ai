42e4795c590bd20691489a5774afe3026791c039
Khunink <khemiga.ink@gmail.com>
2026-07-21 03:05:15 +0000

    return model

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ load_dotenv()
 # ดึงค่า Config มาเก็บในตัวแปร เพื่อให้เรียกใช้ง่ายและปลอดภัย
 API_URL = os.getenv("OPENWEBUI_API_URL")
 API_KEY = os.getenv("OPENWEBUI_API_KEY")
-MODEL_NAME = os.getenv("hu-mate") # ถ้าไม่มีการตั้งค่าจะใช้ชื่อ hu-mate เป็นค่าเริ่มต้น
+MODEL_NAME = os.getenv("MODEL_NAME", "hu-mate") # ถ้าไม่มีการตั้งค่าจะใช้ชื่อ hu-mate เป็นค่าเริ่มต้น
